42517f5e646f01605a7b3042fcba53755a7017d3
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-08 13:42:08 -0300

    refactor: adding icon for delete task

--- src/components/CardTasks.js
+++ src/components/CardTasks.js
@@ -1,11 +1,8 @@
 import { StyleSheet, Text, View, TouchableOpacity  } from 'react-native';
 import Axios from 'axios';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const CardTasks = ({Title, Description, IdTask}) => {
-
-  const deletetask = () => {
-
-  }
+const CardTasks = ({Title, Description, IdTask, onpress}) => {
   
     return(
         <TouchableOpacity style={styles.container}>
@@ -13,6 +10,9 @@ const CardTasks = ({Title, Description, IdTask}) => {
                 <Text style={styles.title}>{Title}</Text>
                 <Text style={styles.description}>{Description}</Text>
             </View>
+            <TouchableOpacity style={styles.iconDelete} onPress={onpress}>
+              <Ionicons name="trash" size={32} color="green" />
+            </TouchableOpacity>
         </TouchableOpacity>
     )
 }
@@ -21,9 +21,11 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: 80,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     borderBottomWidth: 1,
-    borderColor: '#29323C'
+    borderColor: '#29323C',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   card: {
     gap: 5,
@@ -38,6 +40,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#FFFFFF',
     opacity: 0.5
+  },
+  iconDelete: {
+    marginRight: 30
   }
 });
 
